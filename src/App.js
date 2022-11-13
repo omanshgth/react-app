e9599e177199020ae69abdba6e66442fd1ee68c8
Omansh Kumar Singh <omansh.gth@gmail.com>
2022-11-13 23:02:57 +0530

    feat: Added the fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Application
+          Learn React Application latest deployment
         </a>
       </header>
     </div>
