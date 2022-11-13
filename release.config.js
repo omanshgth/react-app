9f83de3d0b96186d9854bde6c004bcbe04adeebd
Omansh Kumar Singh <omansh.gth@gmail.com>
2022-11-13 17:31:46 +0530

    fix: fixed the description

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/omanshgth/react-app",
+    plugins: [
+            '@semantic-release/commit-analyzer', 
+            '@semantic-release/release-notes-generator', 
+            '@semantic-release/npm', 
+            '@semantic-release/github'
+        ]
+}
