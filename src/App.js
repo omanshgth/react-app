15b9350850dac69af38005015f7f37096c5f80b4
Omansh Kumar Singh <omansh.gth@gmail.com>
2022-11-13 15:03:48 +0530

    Updated the deployment to staging server

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Application Building
+          Learn React Application
         </a>
       </header>
     </div>
