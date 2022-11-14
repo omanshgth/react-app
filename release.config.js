6024e1e0f5f45fe9685528d7c8be20c26c87a711
Omansh Kumar Singh <omansh.gth@gmail.com>
2022-11-14 14:51:48 +0530

    fix: fixed the bug for the page

--- release.config.js
+++ release.config.js
@@ -5,6 +5,13 @@ module.exports = {
             '@semantic-release/commit-analyzer', 
             '@semantic-release/release-notes-generator', 
             '@semantic-release/npm', 
-            '@semantic-release/github'
+            ['@semantic-release/github',
+                {assets: [
+                    {path: "build.zip", label: "Build"},
+                    {path: "cover.zip", label: "Cover"}
+                ]
+                }
+                
+            ]
         ]
 }
